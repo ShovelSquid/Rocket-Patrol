9f1761b54c6635743835eb85363c73c406cdddff
Kaelen Cook <47254824+ShovelSquid@users.noreply.github.com>
2022-04-08 14:50:50 -0700

    Initial project commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+console.log("hello there");
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    title: "Rocket Patrol",
+    version: "2.01",
+    author: "Kaelen Cook"
+}
+
+let game = new Phaser.Game(config);
